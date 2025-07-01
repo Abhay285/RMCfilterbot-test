779935009a02280f166e07a731a45dcaca474185
Abhay kaiwartya <abhaykaiwartya2019@gmail.com>
2025-07-01 14:56:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# client.py
+from pyrogram import Client
+from info import API_ID, API_HASH, BOT_TOKEN
 
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="RMCFILTERBOT",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            plugins={"root": "plugins"}
+        )
+
+
+# main.py
 from client import Bot
 
-print("Bot Started 👍 Powered By @RMCBACKUP")
+print("Bot Started 👍 Powered By @VJ_Botz")
 Bot().run()
-
