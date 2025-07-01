0b181449886346556935124d5b62c11730716107
Abhay kaiwartya <abhaykaiwartya2019@gmail.com>
2025-07-01 21:57:35 +0530

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -1,20 +1,35 @@
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from config import DATABASE_URL, DATABASE_NAME
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Database:
     def __init__(self):
-        self._client = MongoClient(DATABASE_URL)
+        self._client = AsyncIOMotorClient(DATABASE_URL)
         self.db = self._client[DATABASE_NAME]
         self.col = self.db["Posts"]
+        self.users_col = self.db["Users"]
+        self.groups_col = self.db["Groups"]
 
-    def insert_post(self, data):
-        return self.col.insert_one(data)
+    async def insert_post(self, data):
+        return await self.col.insert_one(data)
 
-    def search_posts(self, query):
-        return self.col.find(
+    async def search_posts(self, query):
+        return await self.col.find(
             {"$text": {"$search": query}},
             {"score": {"$meta": "textScore"}}
-        ).sort("score", -1)
+        ).sort("score", -1).to_list(None)
 
-# Global database instance
+    async def find_user(self, user_id):
+        return await self.users_col.find_one({"_id": user_id})
+
+    async def update_user(self, user_id, data):
+        return await self.users_col.update_one(
+            {"_id": user_id},
+            {"$set": data},
+            upsert=True
+        )
+
+# Global async database instance
 database = Database()
