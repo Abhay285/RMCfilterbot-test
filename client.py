733fb0d269a5b28a592ed1a625f3ceeae22f61e0
Abhay kaiwartya <abhaykaiwartya2019@gmail.com>
2025-07-01 18:16:27 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,20 +1,10 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-from info import *
 from pyrogram import Client
+from info import API_ID, API_HASH, BOT_TOKEN
 
-class Bot(Client):   
-    def __init__(self):
-        super().__init__(   
-           "vj-post-search-bot",
-            api_id=API_ID,
-            api_hash=API_HASH,           
-            bot_token=BOT_TOKEN,
-            plugins={"root": "plugins"})
-    async def start(self):                        
-        await super().start()  
-        print("Bot Started 🔧 Powered By @RMCBACKUP")   
-    async def stop(self, *args):
-        await super().stop()
+bot = Client(
+    "RMCBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="plugins")
+)
