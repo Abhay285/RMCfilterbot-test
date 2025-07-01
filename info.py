93c61552c825c6a6027b802a11efa21f9e620696
Abhay kaiwartya <abhaykaiwartya2019@gmail.com>
2025-07-01 17:42:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,16 +1,10 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-# Never Give DATABASE_URI And BOT_TOKEN Here If Repo Is Forked Or Public Because This May Lead Account Ban.
-
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", "29034951"))
-API_HASH     = environ.get("API_HASH", "cfc8448de6746a6ccf9dc1812cf1c8f7")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "29034951"))
+API_HASH = environ.get("API_HASH", "cfc8448de6746a6ccf9dc1812cf1c8f7")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002423393168"))
-ADMIN        = int(environ.get("ADMIN", "1328531459"))
-CHANNEL      = environ.get("CHANNEL", "@RMCBACKUP")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002423393168"))
+ADMIN = int(environ.get("ADMIN", "1328531459"))
+CHANNEL = environ.get("CHANNEL", "@RMCBACKUP")
